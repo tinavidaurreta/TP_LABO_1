b3fcb25b055792e6a0ef681a62c714e34effee2b
tinavidaurreta <142612257+tinavidaurreta@users.noreply.github.com>
2025-05-26 11:35:46 -0300

    Menu(1)

--- tpfinal/main.py
+++ tpfinal/main.py
@@ -28,23 +28,16 @@ JUSTO = Receptores(46333111, 'Justo Larguia', fecha_justo, 'M', 1145451111, 'B',
 receptores=[FELIPE, SIMON, JUSTO]
 donantes = [CLARA]
 INCUCAI= Incucai(receptores,donantes,[SWISSMEDICAL,AUSTRAL,ALEMAN,BRITANICO])
-cantidad = int(input('Ingrese cuanta cantidad de pacientes quiere registrar'))
-#En caso de que se ingrese en formato texto
+
+'''cantidad = int(input('Ingrese cuanta cantidad de pacientes quiere registrar'))
 try:
     cantidad = int(cantidad)
 except ValueError:
     print('Debe ingresar un número entero para la cantidad.')
     cantidad = 0
-
 if cantidad > 0:
     for j in range(0,cantidad,1):
         x = input('Ingrese "R" si va a ingresar un paciente receptor o ingrese "D" si va a ingresar un paciente donante:' ).upper()
-        '''try:
-            if x != "R" or x != "D":
-                raise ErrDeChar()
-        except ErrDeChar as e:
-            print(e)
-            x = None'''
         if x == 'R' :
             DNI = input ('Ingrese su DNI:')
             nombre_apellido = input ('Ingrese su nombre y apellido:')
@@ -108,42 +101,100 @@ if cantidad > 0:
                     palabra += caracter  # va construyendo la palabra
                 elif palabra:
                         lista_organos.append(palabra)
-                        palabra = ""  # reinicia para la siguiente palabra
-            # Añadir la última palabra si no termina con espacio
-            if palabra:
+                        palabra = ""  # reinicia para la siguiente palabra            
+            if palabra:# Añadir la última palabra si no termina con espacio
                 lista_organos.append(palabra)
             PACIENTE_DONANTE = Donantes(DNI, nombre_apellido, fecha_str, sexo, telefono, tipo_sangre, centro_salud, 0, 0, lista_organos)
             INCUCAI.registrar_paciente_donante(PACIENTE_DONANTE)
-
     # Faltan excepciones y chequear que agregue bien, hacer mas medicos, mas sedes
-
-
-"""try:
-    #intentar acceder a un atributo exclusivo de receptor
-    organo_requerido = receptores.organo
-
-    centro_donante = donantes.centro_salud
-    centro_receptor = receptores.centro_salud
-
-    #asignar cirujano del centro de salud del donante
-    cirujano = centro_donante.asignar_cirujano()
-    if cirujano is None:
-        print("No se pudo asignar un cirujano para el trasplante.")
-    else:
-        # asignar vehículo para transportar el órgano
-        vehiculo = centro_donante.asignar_vehiculo(centro_receptor)
-        print(f"Vehículo asignado: {type(vehiculo).__name__}")
-        print(f"El medico asignado: {type(cirujano).__name__}")
-
-        # realizar el trasplante
-        exito = centro_donante.realizar_transplante(donantes.fecha_hora_ablacion, vehiculo, cirujano, organo_requerido)
-
-        if exito:
-            print("Trasplante exitoso.")
-            
-        else:
-            print("El trasplante falló.")
-
-except AttributeError:
-    print("El paciente no es receptor, no se puede realizar el trasplante.")
-"""
+'''
+def menu(opcion):
+    match opcion:
+        case 1:
+            cantidad = int(input('Ingrese cuanta cantidad de pacientes quiere registrar'))
+            try:
+                cantidad = int(cantidad)
+            except ValueError:
+                print('Debe ingresar un número entero para la cantidad.')
+                cantidad = 0
+            if cantidad > 0:
+                for j in range(0,cantidad,1):
+                    x = input('Ingrese "R" si va a ingresar un paciente receptor o ingrese "D" si va a ingresar un paciente donante:' ).upper()
+                    if x == 'R' :
+                        DNI = input ('Ingrese su DNI:')
+                        nombre_apellido = input ('Ingrese su nombre y apellido:')
+                        fecha_str = input('Ingrese una fecha de nacimiento en formato DD/MM/AAAA: ')
+                        sexo = input ('Ingrese su F o M segun su sexo:')
+                        telefono = input ('Ingrese su numero de telefono:')
+                        tipo_sangre = input ('Ingrese su tipo de sangre (A / AB / B / 0):').upper()
+                        prioridad = random.randint(1,5)
+                        centro_salud = None
+                        try:
+                            centro_salud_num = int(input('Ingrese su centro de salud afiliado: (1. ALEMAN 2. SWISSMEDICAL 3. AUSTRAL 4. BRITANICO): '))
+                            if centro_salud_num == 1 :
+                                centro_salud = ALEMAN
+                            elif centro_salud_num == 2:
+                                centro_salud = SWISSMEDICAL
+                            elif centro_salud_num == 3:
+                                centro_salud = AUSTRAL 
+                            elif centro_salud_num == 4:
+                                centro_salud = BRITANICO 
+                        except ValueError:
+                            print('Por favor, ingrese un número válido.')
+                        if centro_salud is None:
+                            print('No se pudo asignar un centro de salud válido, se asignará ALEMAN por defecto.')
+                            centro_salud = ALEMAN
+                        patologia = input ('Ingrese su patologia:')
+                        organo = input ('Ingrese el organo necesitante:').lower()
+                        fecha_lista_espera = datetime.now()
+                        PACIENTE_RECEPTOR = Receptores(DNI, nombre_apellido, fecha_str, sexo, telefono, tipo_sangre, centro_salud, fecha_lista_espera, prioridad, patologia, True, organo) # Siempre True hasta que se opere y cambie
+                        INCUCAI.registrar_paciente_receptor(PACIENTE_RECEPTOR)
+                    elif x == 'D':
+                        DNI = input ('Ingrese su DNI:')
+                        nombre_apellido = input ('Ingrese su nombre y apellido:')
+                        fecha_str = input('Ingrese una fecha de nacimiento en formato DD/MM/AAAA: ')
+                        sexo = input ('Ingrese su F o M segun su sexo:')
+                        telefono = input ('Ingrese su numero de telefono:')
+                        tipo_sangre = input ('Ingrese su tipo de sangre (A / AB / B / 0):').upper()
+                        centro_salud = None
+                        fecha_fallecimiento_hora = datetime.now()
+                        fecha_hora_ablacion = 0
+                        try:
+                            centro_salud_num = int(input('Ingrese su centro de salud afiliado: (1. ALEMAN 2. SWISSMEDICAL 3. AUSTRAL 4. BRITANICO): '))
+                            if centro_salud_num == 1 :
+                                centro_salud = ALEMAN
+                            elif centro_salud_num == 2:
+                                centro_salud = SWISSMEDICAL
+                            elif centro_salud_num == 3:
+                                centro_salud = AUSTRAL 
+                            elif centro_salud_num == 4:
+                                centro_salud = BRITANICO 
+                        except ValueError:
+                            print('Por favor, ingrese un número válido.')
+                        if centro_salud is None:
+                            print('No se pudo asignar un centro de salud válido, se asignará ALEMAN por defecto.')
+                            centro_salud = ALEMAN 
+                        organos = input ('Ingrese el/los organos donantes:').lower()
+                        palabra = ""
+                        lista_organos = []
+                        for caracter in organos:
+                            if caracter != " ":
+                                palabra += caracter  # va construyendo la palabra
+                            elif palabra:
+                                    lista_organos.append(palabra)
+                                    palabra = ""  # reinicia para la siguiente palabra            
+                        if palabra:# Añadir la última palabra si no termina con espacio
+                            lista_organos.append(palabra)
+                        PACIENTE_DONANTE = Donantes(DNI, nombre_apellido, fecha_str, sexo, telefono, tipo_sangre, centro_salud, fecha_fallecimiento_hora, fecha_hora_ablacion, lista_organos)
+                        INCUCAI.registrar_paciente_donante(PACIENTE_DONANTE)
+        case 2:
+            print("Elegiste la opción 2")
+        case 3:
+            print("Elegiste la opción 3")
+        case 4:
+            INCUCAI.imprimir_donantes_receptores()
+        case _:
+            print("Opción no válida")
+# Ejemplo de uso
+opcion = int(input("Elige una opción: \n1. Registrar paciente nuevo:\n2. Buscar por centro de salud los pacientes de la lista de espera\n3. Informar la prioridad de un paciente: \n4. Imprimir la lista pacientes donantes y receptores:\n"))
+menu(opcion)
